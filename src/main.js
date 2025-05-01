08cdcf7a2cfbc7722569969f5c55258ee6e5da76
Lisa <84908214+LisaS7@users.noreply.github.com>
2025-05-01 14:56:01 +0100

    Refactoring testing scene to a separate file

--- src/main.js
+++ src/main.js
@@ -1,50 +1,15 @@
 import { loadPyodidePlugin } from "./pyodideRunner.js";
-import { initEditor, runEditorCode } from "./codeEditor.js";
+import { MenuScene } from "./scenes/menuScene.js";
 import { gameConfig } from "../config/game.js";
-import { fontConfig } from "../config/text.js";
-
-let codeText;
-let outputText;
-let editor = null;
+import { TestingScene } from "./scenes/testingScene.js";
 
 const config = {
   type: Phaser.AUTO,
   parent: "game-container",
   ...gameConfig,
-  scene: {
-    preload: preload,
-    create: create,
-    update: update,
-  },
+  scene: [TestingScene],
 };
 const game = new Phaser.Game(config);
 
 // Load pyodide
 await loadPyodidePlugin();
-
-// Preload assets
-function preload() {
-  this.load.image("logo", "https://phaser.io/images/logo.png");
-}
-
-// Create initial game elements
-function create() {
-  // Initialize CodeMirror for the code editor
-  editor = initEditor();
-
-  // Test stuff, will remove later
-  codeText = this.add.text(20, 400, "Hi", fontConfig);
-  outputText = this.add.text(20, 500, "Output: ", fontConfig);
-  const logo = this.add.image(400, 300, "logo");
-  logo.setOrigin(0.5, 0.5);
-
-  // UI Elements
-  document.getElementById("run-button").addEventListener("click", async () => {
-    const result = await runEditorCode(editor);
-    outputText.text = result.output;
-    codeText.text = result.code;
-  });
-}
-
-// Game loop
-function update() {}
---
+++ src/scenes/testingScene.js
@@ -0,0 +1,36 @@
+import { initEditor, runEditorCode } from "../codeEditor.js";
+import { normalFontConfig } from "../../config/text.js";
+
+export class TestingScene extends Phaser.Scene {
+  constructor() {
+    super("TestingScene");
+    this.codeText = null;
+    this.outputText = null;
+    this.editor = null;
+  }
+
+  // Preload assets
+  preload() {
+    // this.load.image("logo", "https://phaser.io/images/logo.png");
+  }
+
+  create() {
+    // Initialize CodeMirror for the code editor
+    this.editor = initEditor();
+
+    // Output test variables on game screen
+    this.codeText = this.add.text(20, 400, "Hi", normalFontConfig);
+    this.outputText = this.add.text(20, 500, "Output: ", normalFontConfig);
+    const logo = this.add.image(400, 300, "logo");
+    logo.setOrigin(0.5, 0.5);
+
+    // UI Elements
+    document
+      .getElementById("run-button")
+      .addEventListener("click", async () => {
+        const result = await runEditorCode(this.editor);
+        this.outputText.text = result.output;
+        this.codeText.text = result.code;
+      });
+  }
+}
