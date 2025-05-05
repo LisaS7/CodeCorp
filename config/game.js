3b95bbc2925c13f252d3226f3bbc71a68f5ff2e2
Lisa <84908214+LisaS7@users.noreply.github.com>
2025-05-05 11:33:21 +0100

    Fixing problems with hex string format

--- config/game.js
+++ config/game.js
@@ -1,10 +1,14 @@
 import Phaser from "../lib/phaser.js";
 
+// Phaser expects color **strings** (e.g., "#000000") for text styles like `color` and `backgroundColor`,
+//       but expects numeric hex (e.g., 0x000000) for graphics and rectangle fills.
+
 const colours = {
   mediumBlue: 0x0077b6,
   lightestGrey: 0xfbfbfb,
   midGrey: 0xd3d3d3,
   midBrown: 0x4a3728,
+  black: 0x000000,
 };
 
 export const gameConfig = {
@@ -17,10 +21,16 @@ export const gameConfig = {
     mode: Phaser.Scale.FIT,
     parent: "game-container",
   },
+  fps: {
+    target: 0.1, // Limiting the frame rate to 30 FPS (default is 60)
+  },
 };
 
 export const menuConfig = {
   itemSpacing: 40,
+  selectedBgColour: colours.mediumBlue,
+  selectedFontColour: colours.black,
+  unselectedFontColour: colours.black,
 };
 
 export const smallDialoguePaneConfig = {
